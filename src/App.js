4de0b5661c5276f1ade5f06ad9c0dcae8bcef511
Yasmine <yasmine.ibrahimi321@gmail.com>
2024-09-16 22:35:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learnn React
         </a>
       </header>
     </div>
