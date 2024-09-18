def503c6f474741a27fe12120ce968416bc663d8
Yasmine <yasmine.ibrahimi321@gmail.com>
2024-09-18 14:15:54 +0200

    routing configured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home';
+import DogGallery from './components/Dog gallery';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learnn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/gallery">Dog Gallery</Link></li>
+         
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<DogGallery />} />
+       
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
