ace7a019c3c81f483253d19d85ebfb73254ce6c7
Yasmine <yasmine.ibrahimi321@gmail.com>
2024-09-18 14:29:08 +0200

    routing configuration update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Home from './components/Home';
 import DogGallery from './components/Dog gallery';
+import DogBreedSearch from './components/Dog breed search';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/gallery">Dog Gallery</Link></li>
+            <li><Link to="/breedsearch">Breed Search</Link></li>
          
           </ul>
         </nav>
@@ -19,7 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<DogGallery />} />
-       
+          <Route path="/breedsearch" element={<DogBreedSearch/>}/>
         </Routes>
       </div>
     </Router>
